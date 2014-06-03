c21aba89e31152b52e689cb4d0b303436cc12907
aju <aju@te.ceaaces>
2014-06-03 12:17:18 -0500

    Incluyendo kriging ordinario

--- R/kriging.R
+++ R/kriging.R
@@ -1,5 +1,5 @@
 #___________________________________________________________________________________________________
-# Creación de Kriging
+# Simple kriging
 kriging_simple<-function( Z, X, x0, k ) {
   K<-NULL
   k0<-NULL
@@ -12,42 +12,6 @@ kriging_simple<-function( Z, X, x0, k ) {
   J<-chol2inv( L )
   Z0<-t( k0 ) %*% J %*% Z
   return( list( Z0 = Z0, K = K, k0 = k0, L = L, J = J ) )
-}
-
-# conjugate_gradient<-function( A, b, x, n, e ) {
-#   r<-A %*% x - b
-#   d<- -r
-#   alpha<- -as.numeric( ( t(r) %*% d ) / ( t(d) %*% A %*% d ) )
-#   x<-x + alpha * d
-#   k<-0
-#   while( k < n && sqrt( t(r)%*%r ) > e ) {
-#     r<- A %*% x - b
-#     beta<-as.numeric( ( t(d) %*% A %*% r ) / ( t(d) %*% d ) )
-#     d<- -r + beta * d
-#     alpha<- -as.numeric( ( t(r) %*% d ) / ( t(d) %*% A %*% d ) )
-#     x<-x + alpha * d
-#     k<-k + 1
-#   }
-#   return( x )
-# }
-
-conjugate_gradient<-function( A, b, x, n, e ) {
-  r<-b - A %*% x 
-  p<-r
-  g<-as.numeric( t(r) %*% r )
-  k<-0
-  while ( k < n & g > e ) {
-    y<-A %*% p
-    alpha<-g / as.numeric( t(y) %*% p )
-    x<-x + alpha * p
-    r<-r - alpha * y
-    g0<-g
-    g<-as.numeric( t(r) %*% r )
-    beta<-g / g0
-    p<-r + beta * p
-    k<-k + 1
-  }
-  return( x )
 }
 
 kriging_simple_cg<-function( Z, X, x0, k, l, n, e ) {
@@ -63,5 +27,37 @@ kriging_simple_cg<-function( Z, X, x0, k, l, n, e ) {
     L<-cbind( L, conjugate_gradient( K, k0[,i], l, n, e ) )
   }
   Z0<-t( L ) %*% Z
+  return( list( Z0 = Z0, K = K, k0 = k0, L = L ) )
+}
+
+#___________________________________________________________________________________________________
+# Ordinary kriging
+kriging_ordinary<-function( Z, U, X, x0, u0, k ) {
+  K<-NULL
+  k0<-NULL
+  d<-dim( X )
+  for ( i in 1:d[2] ) { # i<-1
+    K<-rbind( K, apply( X, c(2), FUN = k, X[,i] ) ) 
+    k0<-rbind( k0, apply( x0, c(2), FUN = k, X[,i] ) )
+  }
+  L<-chol( K )
+  J<-chol2inv( L )
+  Z0<-u0 + t( k0 ) %*% J %*% ( Z - U )
+  return( list( Z0 = Z0, K = K, k0 = k0, L = L, J = J ) )
+}
+
+kriging_ordinary_cg<-function( Z, U, X, x0, u0, k, l, n, e ) {
+  K<-NULL
+  k0<-NULL
+  L<-NULL
+  d<-dim( X )
+  for ( i in 1:d[2] ) { # i<-1
+    K<-rbind( K, apply( X, c(2), FUN = k, X[,i] ) ) 
+    k0<-rbind( k0, apply( x0, c(2), FUN = k, X[,i] ) )
+  }
+  for ( i in 1:dim(x0)[2] ) {
+    L<-cbind( L, conjugate_gradient( K, k0[,i], l, n, e ) )
+  }
+  Z0<-u0 + t( L ) %*% ( Z - U )
   return( list( Z0 = Z0, K = K, k0 = k0, L = L ) )
 }
