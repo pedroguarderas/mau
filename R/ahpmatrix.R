0546017c60c32d4f118bb252ec26b76cb0deef79
drew1d <andrelo1d@hotmail.com>
2014-08-20 13:58:50 -0500

    Writing documentation for AHPmatrix

--- R/ahpmatrix.R
+++ R/ahpmatrix.R
@@ -1,8 +1,12 @@
+#___________________________________________________________________________________________________
+# Función para normalizar matrices triangulares superiores positivas cuadradas 
+# Matrices de comparación en Analytic Hierarchy Process
+
 AHPmatrix<-function( M ) {
   d<-dim(M)
   
-  for ( i in 1:( d[1] - 1 ) ) {
-    for ( j in (i+1):d[2] ) {
+  for ( i in 1:( d[1] - 1 ) ) { # i<-1
+    for ( j in (i+1):d[2] ) { # j<-2
       if ( M[i,j] != 0 ) {
         M[j,i] <- ( 1 / M[i,j] ) 
       } 
@@ -10,7 +14,7 @@ AHPmatrix<-function( M ) {
   }
   rm(i,j)
   cols<-colSums( M )
-  for ( j in 1:d[2] ) {
+  for ( j in 1:d[2] ) { # j<-3
     M[,j] <- M[,j] / cols[j]
   }
   return( list( rowMeans(M), M ) )
