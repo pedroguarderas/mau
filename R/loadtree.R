f7d5ff9bda7ca68fa9cd5504c25011c36027fee1
aju <ajusan@te.ceaaces>
2015-05-21 16:43:53 -0500

    Incluyendo función personalizada para guardar el árbol

--- R/loadtree.R
+++ R/loadtree.R
@@ -57,7 +57,10 @@ write_tree<-function( file, tree, col_id, col_cod, col_nom, col_parent, col_weig
     <default>Nombre</default>
   </key>
   <key id=\"w\" for=\"node\" attr.name=\"weight\" attr.type=\"double\">
-    <default>1.0</default>
+    <default>0.0</default>
+  </key>
+  <key id=\"rw\" for=\"node\" attr.name=\"rweight\" attr.type=\"double\">
+    <default>0.0</default>
   </key>
   <key id=\"lf\" for=\"node\" attr.name=\"leaf\" attr.type=\"double\">
     <default>1.0</default>
@@ -78,6 +81,7 @@ write_tree<-function( file, tree, col_id, col_cod, col_nom, col_parent, col_weig
                     "\t\t<data key=\"c\">", color_node, "</data>\n",
                     "\t\t<data key=\"w\">", ifelse( !is.na( tree[i,col_weight] ), 
                                                     tree[i,col_weight], '' ), "</data>\n",
+                   "\t\t<data key=\"rw\">", "", "</data>\n",
                     "\t\t<data key=\"lf\">", ifelse( !is.na( tree[i,col_weight] ), 
                                                     1, 0 ), "</data>\n",
                     "\t\t<data key=\"f\">", ifelse( !is.na( tree[i,col_weight] ), 
@@ -94,6 +98,7 @@ write_tree<-function( file, tree, col_id, col_cod, col_nom, col_parent, col_weig
                  "\t\t<data key=\"n\">", tree[i,col_nom], "</data>\n",
                  "\t\t<data key=\"c\">", color_node, "</data>\n",
                  "\t\t<data key=\"w\">", "", "</data>\n",
+                 "\t\t<data key=\"rw\">", "", "</data>\n",
                  "\t\t<data key=\"lf\">", 0, "</data>\n",
                  "\t\t<data key=\"f\">", "", "</data>\n",
                  "\t</node>\n", sep = '' )
@@ -103,6 +108,60 @@ write_tree<-function( file, tree, col_id, col_cod, col_nom, col_parent, col_weig
   
   grphxml<-paste( grphxml, nodes, edges, "</graph>\n</graphml>", sep = '' )
   write( grphxml, file = file, append = FALSE ) 
+}
+
+save_tree<-function( tree, file ) {
+  grphxml<-"<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+  <graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"
+  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+  xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns
+  http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">
+  
+  <key id=\"cod\" for=\"node\" attr.name=\"codigo\" attr.type=\"double\"/>
+  <key id=\"c\" for=\"node\" attr.name=\"color\" attr.type=\"string\">
+    <default>yellow</default>
+  </key>
+  <key id=\"n\" for=\"node\" attr.name=\"name\" attr.type=\"string\">
+    <default>Nombre</default>
+  </key>
+  <key id=\"w\" for=\"node\" attr.name=\"weight\" attr.type=\"double\">
+    <default>0.0</default>
+  </key>
+  <key id=\"rw\" for=\"node\" attr.name=\"rweight\" attr.type=\"double\">
+    <default>0.0</default>
+  </key>
+  <key id=\"lf\" for=\"node\" attr.name=\"leaf\" attr.type=\"double\">
+    <default>1.0</default>
+  </key>
+  <key id=\"f\" for=\"node\" attr.name=\"function\" attr.type=\"string\"/>
+
+<graph id=\"G\" edgedefault=\"directed\">
+"
+  nodes<-NULL
+  for( i in 1:length( V(tree)$id ) ) {
+    node<-paste( "\t<node id=\"", V(tree)$id[i], "\">\n",
+                 "\t\t<data key=\"cod\">", V(tree)$codigo[i], "</data>\n",
+                 "\t\t<data key=\"n\">", V(tree)$name[i], "</data>\n",
+                 "\t\t<data key=\"c\">", V(tree)$color[i], "</data>\n",
+                 "\t\t<data key=\"w\">", V(tree)$weight[i], "</data>\n",
+                 "\t\t<data key=\"rw\">", V(tree)$rweight[i], "</data>\n",
+                 "\t\t<data key=\"lf\">", V(tree)$leaf[i], "</data>\n",
+                 "\t\t<data key=\"f\">", V(tree)$'function'[i], "</data>\n",
+                 "\t</node>\n", sep = '' )
+    nodes<-paste( nodes, node, sep = '' )
+  }
+  
+  edges<-NULL
+  for( i in 1:length( E(tree)$id ) ) {
+    edge<-paste( "\t<edge id=\"e", i, "\" ", "directed=\"true\" ", 
+                 "source=\"", V(tree)[ get.edge(tree,i)[1] ]$id, 
+                 "\" target=\"", V(tree)[ get.edge(tree,i)[2] ]$id, "\"/>\n", sep = '' )
+    edges<-paste( edges, edge, sep = '' )
+  }
+  
+  grphxml<-paste( grphxml, nodes, edges, "</graph>\n</graphml>", sep = '' )
+  write( grphxml, file = file, append = FALSE ) 
+  
 }
 
 #___________________________________________________________________________________________________
@@ -120,11 +179,11 @@ sum_weights<-function( tree ) {
 
 divide_weights<-function( tree ) {
   w<-V(tree)$weight
-  for ( i in 1:length( V(tree)$weight ) ) { # i<-leaves[1]
+  for ( i in 1:length( w ) ) { # i<-leaves[1]
     parent<-unlist( neighborhood( tree, 1, V(tree)[i], mode = 'in' ) )
     parent<-parent[ parent != i ]
     if ( length( parent ) == 1 ) {
-      V( tree )[i]$weight<-w[i] / w[parent]
+      V( tree )[i]$rweight<-w[i] / w[parent]
     }
   }
   return( tree )
