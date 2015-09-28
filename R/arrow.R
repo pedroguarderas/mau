f320d51a92bc95c9da1bfaf85ca80f9b0b0196f4
aju <ajusan@te.ceaaces>
2015-09-28 13:54:21 -0500

    Incluyendo funciones para medir relaciones de orden y función de equidad social

---
+++ R/arrow.R
@@ -0,0 +1,28 @@
+
+
+ArrowEquity<-function( U, r ) {
+  n<-ncol(U)
+  m<-nrow(U)
+  a<-NULL
+  if ( m == length( r ) ) {
+    a<-apply( U[,2:n], 2,
+              FUN = function( u, r, m ) return( sum( order( u ) == order( r ) ) / m ), r, m )
+  }
+  rm( n, m )
+  a<-as.data.frame( t( a ) )
+  return( a )
+}
+
+ArrowDiscrepancy<-function( U ) {
+  n<-ncol(U)
+  m<-nrow(U)
+  A<-data.frame()
+  for ( i in 1:m ) {
+    A<-rbind( A, apply( U[,2:n], 1, 
+                        FUN = function( x, y, n ) return( sum( x >= y ) / (n-1) ), U[i,2:n], n ) )
+  }
+  names( A )<-U[,1]
+  rownames( A )<-U[,1]
+  return( A )
+}
+
