1bd21631a45022b5570f748b0a905b0dbcd8246c
drew <drew@drew.drew>
2015-09-29 15:45:52 -0500

    Incluyendo títulos

--- R/pareto.R
+++ R/pareto.R
@@ -11,21 +11,3 @@ pareto<-function( SIM, p, m ) {
   }
 }
 
-# Ejemplo
-
-SIM<-S
-SIM$cod<-NULL
-p<-ncol(SIM)
-m<-nrow(SIM)
-
-pareto(SIM,p,m)
-
-Par<-data.frame( mp = rowMeans( P ) )
-Par<-data.frame( mp = rowSums( P ) )
-
-R<-data.frame( index_name$nom, W = W[,1], WP = W[,972] )
-plot( factor( index_name$nom, levels = index_name$nom ), 
-      W[,1], cex=0.7, pch=16, col='red', type = 'b', ylim = c(0,0.20) )
-points( factor( index_name$nom, levels = index_name$nom ),
-        W[,972], cex=0.7, pch=16, col='orange', type = 'b' )
-
