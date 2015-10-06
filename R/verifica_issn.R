de12f67117af0bece54cc94a6a2fd9522ef223ee
aju <ajusan@te.ceaaces>
2015-10-06 15:54:55 -0500

    Mejora a la función que verifica issn

--- R/verifica_issn.R
+++ R/verifica_issn.R
@@ -1,19 +1,31 @@
 #___________________________________________________________________________________________________
 # Verifica ISSN
 
-verifica_issn<-function( x ) {
-  check<-NULL
-  issn<-unlist( str_split( x,'' ) )
+verifica_issn<-function( x, modify = FALSE ) {
+  check<-FALSE
+  issn<-unlist( str_split( x, ' ' ) )[[1]]
+  
+  if (  modify ) {
+    l<-nchar( issn )
+    if ( l < 8 ) {
+      issn<-paste( paste( rep('0',8-l), collapse = '' ), issn, sep = '' )
+    }
+  }
+    
+  issn<-unlist( str_split( issn,'' ) )
   issn<-issn[ nchar( issn ) == 1 ]
+  issn<-issn[ grepl( '[0-9|X]', issn ) ]
   issn[ issn == 'X' ]<-'10'
   issn<-as.numeric( issn )
   
-  if ( issn[8] == 10 ) {
-    digit<-( sum( issn[1:7] * 8:2 ) + 10 ) %% 11
-    check<-digit == 0
-  } else {
-    digit<-( 11 - sum( issn[1:7] * 8:2 ) %% 11 ) %% 11
-    check<-digit == issn[8]
-  }
+  if ( length( issn ) == 8 ) {
+    if ( issn[8] == 10 ) {
+      digit<-( sum( issn[1:7] * 8:2 ) + 10 ) %% 11
+      check<-digit == 0
+    } else {
+      digit<-( 11 - sum( issn[1:7] * 8:2 ) %% 11 ) %% 11
+      check<-digit == issn[8]
+    }
+  } 
   return( check )
 }
