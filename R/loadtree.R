e1df64f7281b9d44d01be7c571b7172a0fc71c64
aju <ajusan@te.ceaaces>
2015-10-07 20:17:08 -0500

    Corrección en la carga del árbol

--- R/loadtree.R
+++ R/loadtree.R
@@ -21,20 +21,21 @@ load_tree<-function( file, indexfile, col_id, cold_weight, col_func ) {
   return( graph )
 }
 
-load_tree1<-function( file, indexfile, sheet, cols, rows, cols_index ) {
+load_tree1<-function( file, index_file, sheet, cols, rows, cols_index ) {
   graph<-read.xlsx( file = file, sheetIndex = sheet, startRow = rows[1], endRow = rows[2],
-                    colIndex = cols, colClasses = c('integer','character', 'integer' ) )
+                    colIndex = cols, colClasses = c('integer','character', 'integer', 'integer' ) )
   colnames( graph )<-c('id','nom','parent','cod')
   graph<-merge( graph, subset( graph, select = c('id','nom') ),
                 by.x = 'parent', by.y = 'id', all.x = TRUE )
   colnames( graph )<-c( 'id_parent', 'id', 'nom', 'cod', 'parent' )
   
-  index<-indexfile
+  index<-index_file[,cols_index]
   colnames( index )<-c('cod','function','weight')
   graph<-merge( graph, index, by = 'cod', all.x = TRUE  )
   graph<-graph[ ,c( 'id', 'id_parent', 'cod', 'nom', 'parent', 'function', 'weight' ) ]
   graph<-graph[ order( graph$id ), ]
   graph$id<-paste( 'n', graph$id, sep = '' )
+  rm( sheet, cols, rows, cols_index )
   rownames( graph )<-NULL
   return( graph )
 }
