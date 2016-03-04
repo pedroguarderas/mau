a6819be617ac6cc3eb37e3e2d93b014b4f526ede
aju <ajusan@te.ceaaces>
2016-03-04 16:28:40 -0500

    Correcciones para algoritmo de k-medias

--- R/kmeans.R
+++ R/kmeans.R
@@ -1,11 +1,11 @@
-distancia<-function( X, M ) {
+distancia<-function( X, d ) {
   n<-ncol( X )
   m<-nrow( X )
   
-  D<-X %*% M %*% t( X )
+  D<-matrix( 0, m, m )
   for ( i in 1:(m-1) ) {
     for ( j in (i+1):m ) {
-      D[i,j]<-D[i,i] + D[j,j] - 2 * D[i,j]
+      D[i,j]<-d( X[i,], X[j,])
       if ( i != j ) {
         D[j,i]<-D[i,j]
       }
@@ -15,25 +15,24 @@ distancia<-function( X, M ) {
   return( D )
 }
 
-distancia.center<-function( X, C, M ) {
+distancia.center<-function( X, C, d ) {
   n<-nrow( C )
   m<-nrow( X )
   
   D<-matrix( 0, m, n )
   for ( i in 1:m ) {
     for ( j in 1:n ) {
-      D[i,j]<-( X[i,] - C[j,] ) %*% ( M %*% ( X[i,] - C[j,] ) )
+      D[i,j]<-d( X[i,], C[j,] )
     }
   }
-  diag(D)<-0
   return( D )
 }
 
 choose.min<-function( x ) {
-  return( sample( which.min( x ), size = 1 ) )
+  return( which.min( x )[1] )
 }
 
-Kmeans<-function( X, M, I = 10, N = 5 ) {
+Kmeans<-function( X, d, I = 10, N = 5 ) {
   K<-data.frame( X )
   n<-nrow( K )
   m<-ncol( K )
@@ -41,13 +40,14 @@ Kmeans<-function( X, M, I = 10, N = 5 ) {
   C<-as.matrix( K[C,] )
   
   for ( i in 1:I ) {
-    D<-distancia.center( X, C, M )
+    D<-distancia.center( X, C, d )
     K$cluster<-apply( D, 1, FUN = choose.min )
     C<-melt( K, id.vars = 'cluster' )
     C<-aggregate( value ~ cluster + variable, C, mean )
-    C<-dcast( data = C, cluster ~ variable, value.var = 'value')
+    C<-dcast( data = C, cluster ~ variable, value.var = 'value' )
     C$cluster<-NULL
     C<-as.matrix( C )
   }
   return( list( cluster = K, centroid = C, distnace = D ) )
 }
+
