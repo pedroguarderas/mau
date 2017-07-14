2f41df12a3623b862db0ae52cd17cb39073b0537
pedroguarderas <pedro.guarderas@webrisko.com>
2017-07-14 13:16:42 -0500

    Improving weight simulation for data frames

--- R/Simulate.Weights.R
+++ R/Simulate.Weights.R
@@ -30,7 +30,7 @@ Sim.Weights<-function( n, utilities, alpha ) {
   colnames( W )<-paste( 'w', 1:n, sep = '' )
   
   M<-ncol( utilities )
-  S<-as.matrix( utilities[ , 2:M ] )
+  S<-as.matrix( utilities[ , 2:M, with = FALSE ] )
   S<-S %*% W  
   S<-cbind( utilities[,1,with = FALSE], S )
   setnames( S, 1:(n+1), c( 'id', paste( 's', 1:n, sep = '' ) ) )
@@ -61,6 +61,8 @@ Sim.Weights<-function( n, utilities, alpha ) {
 #' constraints<-list( list( c(1,2), 0.7 ), 
 #'                    list( c(3,4), 0.3 ) )
 #' S<-Sim.Const.Weights( n, utilities, alpha, constraints )
+#' plot.S<-Plot.Simulation.Weight( S, title = 'Simulations', xlab = 'ID', ylab = 'Utility' ) 
+#' plot( plot.S )
 #' @importFrom  gtools rdirichlet
 #' @export
 Sim.Const.Weights<-function( n, utilities, alpha, constraints ) {
@@ -78,7 +80,7 @@ Sim.Const.Weights<-function( n, utilities, alpha, constraints ) {
   colnames( W )<-paste( 'w', 1:n, sep = '' )
   
   M<-ncol( utilities )
-  S<-as.matrix( utilities[ , 2:M ] )
+  S<-as.matrix( utilities[ , 2:M, with = FALSE ] )
   S<-S %*% W  
   S<-cbind( utilities[,1,with = FALSE], S )
   setnames( S, 1:(n+1), c( 'id', paste( 's', 1:n, sep = '' ) ) )
